3e79b8c82d903f9f2c137e3a169703ee4aab2947
lyret <viktor@lyresten.se>
2020-05-27 14:59:56 +0200

    Mobb-programmering!
    Co-authored-by: Stefan Gillsbro Carrillo <gist19zf@student.ju.se>
    Co-authored-by: Roman Matvichuk <diviziy@hotmail.com>
    Co-authored-by: Aslan Khadizov <Aslan_Argun@hotmail.com>
    Co-authored-by: Vytautas Skergelza <skvy19ak@student.ju.se>
    Co-authored-by: fonto1 <fonto_85@hotmail.com>
    Co-authored-by: Simon Jansson <Simonjansson92@gmail.com>
    Co-authored-by: SusanneJT <susanne.trygg@gmail.com>
    Co-authored-by: Martin Elvin <38648364+MarElv@users.noreply.github.com>
    Co-authored-by: hoss_sony@hotmail.com <hoss_sony@hotmail.com>
    Co-authored-by: Szigeti817 <ireneszabo74@gmail.com>
    Co-authored-by: Jimmy Johansson <jim-joh@outlook.com>
    Co-authored-by: treox <tero.viitala@outlook.com>
    Co-authored-by: Stefan G. <stefan.gillsbro@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+//  - [X] 4 knappar - En knapp komponent
+//  - [ ] Start-knapp komponent
+//  - [ ] App/Spelet komponent
+
+// App/Spelet tillstånd:
+// - [X] Vilka knappar man har tryckt på []
+// - [X] Vilka knappar man ska trycka på []
+// - [X] Har vi tryckt på rätt/fel knapp function
+// - [ ] Igång/aktivt spel boolean
+// - [ ] Poängen number
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const colors = ["green", "red", "blue", "yellow"];
+    const [pressed, setPressed] = useState([])
+    const [correctList, setCorrectList] = useState(["green"]);
+    const [score, setScore] = useState(0)
+    useEffect(() => {
+        if (pressed.length === correctList.length) {
+            let correct = true;
+            for (let i = 0; i < correctList.length; i++) {
+                if (correctList[i] !== pressed[i]) {
+                    correct = false;
+                }
+            }
+            if (correct) {
+				setScore(score + 1);
+				setPressed([]);
+				setCorrectList(addRandomToList(correctList));
+			}
+			else {
+				setScore(0);
+				setPressed([]);
+				setCorrectList(["green"]);
+			}
+        }
+    }, [pressed]);
+
+    useEffect(() => {
+		if (score == 0) {
+			alert("Nytt spel startat");
+		} else {
+			alert("Du har " + score + " poäng")
+		}
+		alert("Tryck på: " + JSON.stringify(correctList));
+
+    }, [score, correctList])
+
+    return (
+        <div>
+            {colors.map(color => (
+                <SimonButton
+                    color={color}
+                    text={color + 'Button'} action={() => setPressed([...pressed, color])} />
+            ))
+            }
+
+        </div>
+    );
 }
 
 export default App;
+
+
+const addRandomToList = (previousList) => {
+    const listOfColors = ['red', 'green', 'blue', 'yellow']
+	const randomColor = listOfColors[Math.floor(Math.random() * listOfColors.length)]
+	return [...previousList, randomColor];
+}
+
+const SimonButton = props => {
+    return (
+        <button style={{ backgroundColor: props.color }} onClick={props.action} id={props.id} >
+            {props.text}
+        </button>
+    );
+}
+
